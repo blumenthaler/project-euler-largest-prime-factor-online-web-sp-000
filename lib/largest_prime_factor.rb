86cd0d07518d678ca39130736645d107516f1e0b
blumenthaler <blumenthaleralex@gmail.com>
2021-02-12 16:58:16 -0500

    commit tree 9a087dda94f44f0f0a184a260f773d13f912c17e

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,26 @@
 # Enter your procedural solution here!
+def largest_prime_factor(limit)
+    array = []
+    i = 2
+    while i < limit
+        if limit % i === 0
+            array << i
+        end
+        break if i > limit
+        i += 1
+    end
+    largest_prime = 0
+    array.each do |int|
+        is_prime = true
+        j = 2
+        while j < int 
+            if int % j == 0
+                is_prime = false
+            end
+            break if j > int
+            j += 1
+        end
+        largest_prime = int if is_prime and int > largest_prime
+    end
+    return largest_prime
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,13 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor 
+    
+    def initialize(top)
+        @top = top
+        @number = 
+    end
+
+    def largest_prime_factor
+        
+    end
+
+end
